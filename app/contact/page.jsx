4601532afde265677bd81a1835b64034d8e5830c
NoScopeJedi83 <atharvgro2003@gmail.com>
2024-07-18 12:36:04 +0530

    first commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
               <div className="xl:w-[54%] order-2 xl:order-none">
                 <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                   <h3 className="text-4xl text-accent">Want to work together?</h3>
-                  <p>Have a project in mind? Drop me a mail at my email and let's work the magic out of it.</p>
+                  <p>Have a project in mind? Drop me a mail at my email and lets work the feces out of it.</p>
                   {/* input */}
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <Input type="text" placeholder="First Name"/>
