70fa971bacba7e7a35769ef4c46942e06a334074
NoScopeJedi83 <atharvgro2003@gmail.com>
2024-07-19 13:09:22 +0530

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ const Home = () => {
               </p>
               {/* btn and socials */}
               <div className="flex flex-col xl:flex-row items-center gap-8">
-                <a href="https://drive.google.com/file/d/1zZAOtNm1tA-DEnwHN3G9jUzMK6W66Oel/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1TMTbsW4q8jn5hSSNe2haujIVrleLO-_1/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                   <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                     <span>Download CV</span>
                     <FiDownload className="text-xl" />
