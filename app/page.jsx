0ed741be321b24c6eef2487bedc01d3a62ab2214
NoScopeJedi83 <atharvgro2003@gmail.com>
2025-01-11 09:48:21 +0530

    resume change 85 score

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ const Home = () => {
               </p>
               {/* btn and socials */}
               <div className="flex flex-col xl:flex-row items-center gap-8">
-                <a href="https://drive.google.com/file/d/1RjXszXIRHh1eL4q9uleMkOuzQ006mrJe/view?usp=sharing">
+                <a href="https://drive.google.com/file/d/17Dtfj1SjE_eTrxlgQaz3OUNSlnSLCOBP/view?usp=sharing">
                   <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                     <span>Download CV</span>
                     <FiDownload className="text-xl" />
